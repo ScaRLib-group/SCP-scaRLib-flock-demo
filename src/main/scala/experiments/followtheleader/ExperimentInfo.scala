71d1a1be3c7342294ec7b3d08a12f84772ccd53d
Davide Domini <davide.domini@unibo.it>
2024-04-02 16:23:57 +0200

    fix: remove useless experiment

--- src/main/scala/experiments/followtheleader/ExperimentInfo.scala
+++
@@ -1,7 +0,0 @@
-package experiments.followtheleader
-
-object ExperimentInfo {
-  val episodes = 500
-  val episodeLength = 100
-  val hiddenSize = 64
-}
--- src/main/scala/experiments/followtheleader/StateInfo.scala
+++
@@ -1,6 +0,0 @@
-package experiments.followtheleader
-
-object StateInfo {
-  val neighborhood: Int = 5
-  val encoding: Int = 2
-}
